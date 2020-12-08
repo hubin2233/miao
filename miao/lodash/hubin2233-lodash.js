ed43ca79fe9eda4f73aec2b43e3db20b4030952e
hubin <1050256244@qq.com>
2020-12-08 15:07:02 +0800

    update

---
+++ miao/lodash/hubin2233-lodash.js
@@ -0,0 +1,11 @@
+var  hubin2233 = {
+  compact:function(ary){
+    var result = []
+    for(var i = 0;i<ary.length;i++){
+      if(!ary[i]){
+        result.push(ary[i])
+      }
+    }
+    return result
+  }
+}
