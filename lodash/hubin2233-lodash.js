cda199598a3b38293bc69bf575e9067227f2496b
hubin <1050256244@qq.com>
2020-12-08 16:56:38 +0800

    1656

--- lodash/hubin2233-lodash.js
+++ lodash/hubin2233-lodash.js
@@ -6,6 +6,13 @@ var hubin2233 = function(){
   // 如果array 无法被分割成全部等长的区块，那么最后剩余的元素将组成一个区块。
   // 参数：array（Array）需要处理的数组
   //       [size=1]（numbe）每个数组区块的长度
+  function chunk(array,size=1){
+    var result = []
+    for(var i=0,j=0;i<array.length;i+=size,j++){
+      result[j] = array.slice(i,(i+size))
+    }
+    return result
+  }
 
 // 创建一个新数组，包含原数组中所有的非假值元素。
 // 例如false, null, 0, "", undefined, 和 NaN 都是被认为是“假值”。
@@ -18,7 +25,6 @@ var hubin2233 = function(){
       } 
     }
     return result 
-    // 测试
   }
 
   return {
